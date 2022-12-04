dbf6d3ae75906c28f5e63fdbc097862f94785fc2
Edward <gameeye@protonmail.com>
2022-12-04 21:37:24 +0100

    Navbar erstellt
    Es wurde eine Navigationsleiste dem Kopf der Webseite hinzugefügt

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,26 +8,42 @@
         </span>
         <h1 class="text-xl">POTWARMER</h1>
       </div>      
-      <ul class="md:flex md:items-center ">
-        <li class="md:mx-4" v-for="link in Links">
+
+      <span @click="MenuOpen()" class="absolute md:hidden right-6 top-1.5 cursor-pointer text-4xl">
+        <i :class="[open ? 'bi bi-x' : 'bi bi-list']"></i>
+      </span>
+
+      <ul class="md:flex md:items-center md:px-0 px-10 md:pb-0 pb-10 md:static absolute bg-gray-900 md:w-auto w-full top-14 duration-700 ease-in" :class="[open ? 'left-0' : 'left-[-100%]']">
+        <li class="md:mx-4 md:my-0 my-6" v-for="link in Links">
           <a :href="link.link" class="text-x1 hover:text-green-500">{{link.name}}</a>
         </li>
+        <Button>Get started</Button>
       </ul>
     </div>
 
 </template>
 
 <script>
+import Button from './Button.vue'
+import { ref } from 'vue'
 export default {
+  components: {
+    Button
+  },
 
   setup(){
+    let open = ref(false)
     let Links = [
       {name: "Home" , link : "#"},
       {name: "Info" , link : "#"},
       {name: "Produkt" , link : "#"},
       {name: "Contact" , link : "#"},
     ]
-    return{Links}
+    function MenuOpen(){
+      open.value = !open.value
+    }
+
+    return{Links, open, MenuOpen}
   }
 }
 </script>
